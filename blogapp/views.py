1da3ac21ab8bdcbcf6244739e4af02a21db4fa8b
AlexxS <alexx.fr-man@yandex.ru>
2023-09-22 17:32:51 +0300

    add logging

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+import logging
 from .models import Article
+
+logger = logging.getLogger(__name__)
 
 
 def blog_index(request):
+    logger.info('Это информационное сообщение')
     articles = Article.objects.all()
     return render(request, template_name="articles.html", context={"articles": articles})
 
 
 def blog_detail(request, slug):
+    logger.info('Это информационное сообщение')
     article = get_object_or_404(Article, slug=slug)
     return render(request, template_name="article_detail.html", context={"article": article})
--- staticpagesapp/views.py
+++ staticpagesapp/views.py
@@ -1,13 +1,19 @@
+import logging
+
 from django.shortcuts import render
 from .models import StaticPage
+
+logger = logging.getLogger(__name__)
 
 
 # Create your views here.
 def index(request):
+    logger.info('Это информационное сообщение')
     index_info = StaticPage.objects.get(id=1)
     return render(request, template_name="index.html", context={"index_info": index_info})
 
 
 def about(request):
+    logger.info('Это информационное сообщение')
     about_info = StaticPage.objects.get(id=2)
     return render(request, template_name="about.html", context={"about_info": about_info})
