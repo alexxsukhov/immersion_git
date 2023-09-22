57c77397a099df96c5dcb9f81391f9bf6e87f694
AlexxS <alexx.fr-man@yandex.ru>
2023-09-22 16:57:51 +0300

    modified static info pages

--- staticpagesapp/views.py
+++ staticpagesapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import StaticPage
 
 
 # Create your views here.
 def index(request):
-    return render(request, template_name="index.html")
+    index_info = StaticPage.objects.get(id=1)
+    return render(request, template_name="index.html", context={"index_info": index_info})
 
 
 def about(request):
