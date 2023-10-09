ef44208f084b8691a284185c9fcc9e3e57e8ad58
Alexx S <alexx.fr-man@yandex.ru>
2023-10-09 18:03:04 +0300

    add task for seminar 4

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -5,4 +5,6 @@ from shopapp import views
 urlpatterns = [
     path('', views.index_shop, name='index_shop'),
     path('product/<slug:slug>/', views.detail_product, name='detail_product'),
+    path('category/<slug:slug>/', views.detail_category, name='detail_category'),
+    path('product/edit/<int:pk>/', views.edit_product, name='edit_product'),
 ]
